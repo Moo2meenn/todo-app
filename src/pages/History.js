74b13e2c8882d6087f5e3537aa22f7426b7632d3
Moo2meenn <momenazher57@gmail.com>
2023-07-18 15:36:19 +0300

    Wrapped tasks cards inside scrollable div

--- src/pages/History.js
+++ src/pages/History.js
@@ -7,17 +7,20 @@ const History = ({ history }) => {
   return (
     <StyledDiv>
       <h1>History</h1>
-      {history
-        .map((task) => (
-          <HistoryTask
-            history={history}
-            name={task.taskName}
-            time={task.deleteTime}
-            status={task.status}
-            key={task.id}
-          />
-        ))
-        .reverse()}
+      <InnerDiv>
+        {history.length === 0 ? <h2>There is no log history!</h2> : null}
+        {history
+          .map((task) => (
+            <HistoryTask
+              history={history}
+              name={task.taskName}
+              time={task.deleteTime}
+              status={task.status}
+              key={task.id}
+            />
+          ))
+          .reverse()}
+      </InnerDiv>
     </StyledDiv>
   );
 };
@@ -26,6 +29,14 @@ const StyledDiv = styled.div`
   background-color: darkgray;
   margin-left: 15%;
   height: 100vh;
+`;
+
+const InnerDiv = styled.div`
+  height: 75vh;
+  width: 75%;
+  overflow: auto;
+  margin: 3rem;
+  padding: 0.1rem;
 `;
 
 export default History;
--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -7,19 +7,22 @@ const Tasks = ({ tasks, setTasks, history, setHistory }) => {
   return (
     <StyledDiv>
       <CreateTask tasks={tasks} setTasks={setTasks} />
-      {tasks
-        .map((task) => (
-          <Task
-            setTasks={setTasks}
-            tasks={tasks}
-            taskName={task.name}
-            id={task.id}
-            key={task.id}
-            history={history}
-            setHistory={setHistory}
-          />
-        ))
-        .reverse()}
+      <InnerDiv>
+        {tasks.length === 0 ? <h2>Add some tasks to show up here!</h2> : null}
+        {tasks
+          .map((task) => (
+            <Task
+              setTasks={setTasks}
+              tasks={tasks}
+              taskName={task.name}
+              id={task.id}
+              key={task.id}
+              history={history}
+              setHistory={setHistory}
+            />
+          ))
+          .reverse()}
+      </InnerDiv>
     </StyledDiv>
   );
 };
@@ -29,6 +32,14 @@ const StyledDiv = styled.div`
   margin-left: 15%;
   padding: 1rem;
   height: 100vh;
+`;
+
+const InnerDiv = styled.div`
+  height: 75vh;
+  width: 75%;
+  overflow: auto;
+  margin: 3rem;
+  padding: 0.1rem;
 `;
 
 export default Tasks;
